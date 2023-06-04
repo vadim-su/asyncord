f9e4fa6bc31dc47b4ef814822266f5fd8f40c357
Vadim Suharnikov <vsuharnikov@gmail.com>
2023-06-04 11:07:20 +0000

    Add tests for command options

--- tests/integration/test_commands.py
+++ tests/integration/test_commands.py
@@ -11,36 +11,113 @@ TEST_APP_ID = '934564225769148436'
 
 
 @pytest.fixture()
-async def commands(client: RestClient):
+async def commands_res(client: RestClient):
     return client.applications.commands(TEST_APP_ID)
 
 
-async def test_create_command(commands: BaseCommandResource):
+async def test_create_command(commands_res: BaseCommandResource):
     command_data = CreateApplicationCommandData(
         type=ApplicationCommandType.CHAT_INPUT,
         name='test-command',
         name_localizations={'en-US': 'test'},
         description='test command description',
         description_localizations={'en-US': 'Test Command Description'},
+        options=[
+            ApplicationCommandOption(
+                type=AppCommandOptionType.STRING,
+                name='test-option',
+                description='test option description',
+            ),
+            ApplicationCommandOption(
+                type=AppCommandOptionType.INTEGER,
+                name='test-option-2',
+                description='test option description 2',
+                required=True,
+            ),
+            ApplicationCommandOption(
+                type=AppCommandOptionType.STRING,
+                name='test-option-3',
+                description='test option description 3',
+                choices=[
+                    ApplicationCommandOptionChoice(
+                        name='test-choice-1',
+                        value='test-value-1',
+                    ),
+                    ApplicationCommandOptionChoice(
+                        name='test-choice-2',
+                        value='test-value-2',
+                    )
+                ],
+            )
+        ],
     )
-    command = await commands.create(command_data)
+
+    # check that the first option is a required integer
+    assert command_data.options[0].type == AppCommandOptionType.INTEGER
+
+    command = await commands_res.create(command_data)
 
     assert command.name == 'test-command'
     assert command.description == 'test command description'
-    await commands.delete(command.id)
+    await commands_res.delete(command.id)
 
 
-# TODO: #9 Add test for creating a command with options and choices
+async def test_create_subcommand_group(commands_res: BaseCommandResource):
+    command_data = CreateApplicationCommandData(
+        type=ApplicationCommandType.CHAT_INPUT,
+        name='test-command-group',
+        description='test command description',
+        options=[
+            ApplicationCommandOption(
+                type=AppCommandOptionType.SUB_COMMAND_GROUP,
+                name='test-subcommand-group',
+                description='test subcommand group description',
+                options=[
+                    ApplicationCommandOption(
+                        type=AppCommandOptionType.SUB_COMMAND,
+                        name='test-subcommand',
+                        description='test subcommand description',
+                        options=[
+                            ApplicationCommandOption(
+                                type=AppCommandOptionType.INTEGER,
+                                name='test-option',
+                                description='test option description',
+                                required=True,
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    command = await commands_res.create(command_data)
+
+    assert command.name == 'test-command-group'
+    assert command.description == 'test command description'
+    await commands_res.delete(command.id)
 
 
-async def test_get_command_list(commands: BaseCommandResource):
-    assert await commands.get_list()
+async def test_get_command_list(commands_res: BaseCommandResource):
+    assert await commands_res.get_list()
 
 
-async def test_get_command(commands: BaseCommandResource):
-    commands_list = await commands.get_list()
-    command = await commands.get(commands_list[0].id)
+async def test_get_command(commands_res: BaseCommandResource):
+    commands_list = await commands_res.get_list()
+    command = await commands_res.get(commands_list[0].id)
 
     assert commands_list[0].id == command.id
     assert commands_list[0].name == command.name
     assert commands_list[0].description == command.description
+
+
+async def test_convert_command_to_create_command(commands_res: BaseCommandResource):
+    commands_list = await commands_res.get_list()
+    command = await commands_res.get(commands_list[0].id)
+
+    command_data = CreateApplicationCommandData.from_command(command)
+    assert command_data.name == command.name
+    assert command_data.description == command.description
+    assert command_data.type == command.type
+    assert command_data.options == command.options
+    assert command_data.default_permission == command.default_permission
