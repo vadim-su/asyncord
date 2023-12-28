1cd9dd03143f65daf63d3cfca63f8478f01e36ce
Vadim Suharnikov <vsuharnikov@gmail.com>
2023-12-28 17:46:22 +0000

    Add FallbackEnum class for handling unknown values in API responses

---
+++ asyncord/fbenum.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from typing import Final
+
+UNKNOWN: Final[str] = 'UNKNOWN'
+"""Fallback name for unknown values."""
+
+
+class FallbackEnum(Enum):
+    """Enum that returns a pseudo-member for unknown values.
+
+    This is useful for enums that are used to represent values from an API.
+    Sometimes, the API will return a value that is not in the enum (and docs).
+    """
+
+    @classmethod
+    def _missing_(cls, value):
+        """Return a pseudo-member for the given value.
+
+        This is called by the enum metaclass when no other member matches.
+        See how enum.Flag handles unknown values.
+        """
+        if cls._member_type_ is object:  # type: ignore
+            pseudo_member = object.__new__(cls)
+        else:
+            pseudo_member = cls._member_type_.__new__(cls, value)  # type: ignore
+        if not hasattr(pseudo_member, '_value_'):
+            pseudo_member._value_ = value
+        pseudo_member._name_ = 'UNKNOWN'
+        return pseudo_member
---
+++ tests/test_fbenum.py
@@ -0,0 +1,23 @@
+from asyncord.fbenum import FallbackEnum
+
+
+def test_get_unknow_item():
+    class TestFBEnum(FallbackEnum):
+        A = 1
+        B = 2
+
+    item_a = TestFBEnum(1)
+    assert item_a is TestFBEnum.A
+    assert item_a.value == TestFBEnum.A.value
+
+    unknown_item = TestFBEnum(3)
+    assert isinstance(unknown_item, TestFBEnum)
+    assert unknown_item.name == "UNKNOWN"
+    assert unknown_item.value == 3
+
+    unknown_item = TestFBEnum(35)
+    assert isinstance(unknown_item, TestFBEnum)
+    assert unknown_item.name == "UNKNOWN"
+    assert unknown_item.value == 35
+    assert unknown_item in TestFBEnum
+    assert list(TestFBEnum) == [TestFBEnum.A, TestFBEnum.B]
