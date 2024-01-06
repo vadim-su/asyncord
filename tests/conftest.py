736ac619b659b44a7f8b6064f5eb16ae4663c8ac
Vadim Suharnikov <vsuharnikov@gmail.com>
2024-01-06 08:51:57 +0000

    Make integration tests enabled by default

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 import os
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Final
 
 import pytest
+
+INTEGRATION_TEST_DIR: Final[Path] = Path(__file__).parent / 'integration'
 
 
 @dataclass
@@ -23,25 +27,22 @@ class IntegrationTestData:
 @pytest.fixture(scope='session')
 def integration_data() -> IntegrationTestData:
     token = os.environ.get('ASYNCORD_TEST_TOKEN')
-    if not token:
-        pytest.skip('ASYNCORD_TEST_TOKEN environment variable is not set')
-    try:
-        return IntegrationTestData(
-            token=token,
-            channel_id=os.environ['ASYNCORD_TEST_CHANNEL_ID'],
-            voice_channel_id=os.environ['ASYNCORD_TEST_VOICE_CHANNEL_ID'],
-            guild_id=os.environ['ASYNCORD_TEST_GUILD_ID'],
-            user_id=os.environ['ASYNCORD_TEST_USER_ID'],
-            message_id=os.environ['ASYNCORD_TEST_MESSAGE_ID'],
-            member_id=os.environ['ASYNCORD_TEST_MEMBER_ID'],
-            custom_emoji=os.environ['ASYNCORD_TEST_CUSTOM_EMOJI'],
-            guild_prefix_to_delete=os.environ['ASYNCORD_TEST_GUILD_PREFIX_TO_DELETE'],
-            app_id=os.environ['ASYNCORD_TEST_APP_ID'],
-            role_id=os.environ['ASYNCORD_TEST_ROLE_ID'],
-            user_to_ban=os.environ['ASYNCORD_TEST_USER_TO_BAN'],
-        )
-    except KeyError as err:
-        pytest.skip(f"'{err.args[0]}' environment variable is not set")
+    if token is None:
+        raise RuntimeError('ASYNCORD_TEST_TOKEN env variable is not set')
+    return IntegrationTestData(
+        token=token,
+        channel_id=os.environ['ASYNCORD_TEST_CHANNEL_ID'],
+        voice_channel_id=os.environ['ASYNCORD_TEST_VOICE_CHANNEL_ID'],
+        guild_id=os.environ['ASYNCORD_TEST_GUILD_ID'],
+        user_id=os.environ['ASYNCORD_TEST_USER_ID'],
+        message_id=os.environ['ASYNCORD_TEST_MESSAGE_ID'],
+        member_id=os.environ['ASYNCORD_TEST_MEMBER_ID'],
+        custom_emoji=os.environ['ASYNCORD_TEST_CUSTOM_EMOJI'],
+        guild_prefix_to_delete=os.environ['ASYNCORD_TEST_GUILD_PREFIX_TO_DELETE'],
+        app_id=os.environ['ASYNCORD_TEST_APP_ID'],
+        role_id=os.environ['ASYNCORD_TEST_ROLE_ID'],
+        user_to_ban=os.environ['ASYNCORD_TEST_USER_TO_BAN'],
+    )
 
 
 @pytest.fixture(scope='session')
@@ -53,18 +54,38 @@ def pytest_addoption(parser):
     parser.addoption(
         '--run-limited', action='store_true', default=False, help='run limited tests',
     )
+    parser.addoption(
+        '--disable-integration', action='store_true', default=False, help='disable integration tests',
+    )
 
 
 def pytest_configure(config):
     config.addinivalue_line('markers', 'limited: mark test as api limited')
+    config.addinivalue_line('markers', 'integration: mark test as integration')
 
 
-def pytest_collection_modifyitems(config, items):
-    if config.getoption('--run-limited'):
-        return
+def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]):
+    """Mark integration tests and skip others if needed."""
+    # Mark integration tests first
+    for item in items:
+        if item.path.is_relative_to(INTEGRATION_TEST_DIR):
+            item.add_marker(pytest.mark.integration)
+
+    # Skip tests if needed
+    run_limited = config.getoption('--run-limited')
+    disable_integration = config.getoption('--disable-integration')
+
     skip_limited = pytest.mark.skip(
         reason='This test has api limitations. Can be run with --run-limited option',
     )
+
+    skip_integration = pytest.mark.skip(
+        reason='This test is integration test. Can be run with --run-integration option',
+    )
+
     for item in items:
-        if 'limited' in item.keywords:
+        if not run_limited and 'limited' in item.keywords:
             item.add_marker(skip_limited)
+
+        if disable_integration and 'integration' in item.keywords:
+            item.add_marker(skip_integration)
