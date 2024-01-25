d6c20e5fa01bc6c68452685e12e30f44d597f96b
Vadim Suharnikov <vsuharnikov@gmail.com>
2024-01-25 21:02:32 +0000

    Update event dispatcher

--- asyncord/gateway/dispatcher.py
+++ asyncord/gateway/dispatcher.py
@@ -1,5 +1,7 @@
 """This module defines the EventDispatcher class, which dispatches events to registered handlers."""
 
+from __future__ import annotations
+
 import logging
 from collections import defaultdict
 from collections.abc import Awaitable, Callable, MutableMapping
@@ -22,19 +24,13 @@ EVENT_T = TypeVar('EVENT_T', bound=GatewayEvent)
 type EventHandlerType[EVENT_T: GatewayEvent] = Callable[Concatenate[EVENT_T, ...], Awaitable[None]]
 """Type alias for an event handler."""
 
-type _HandlersMutMapping[EVENT_T: GatewayEvent] = MutableMapping[
-    type[EVENT_T],
-    list[EventHandlerType[EVENT_T]],
-]
-"""Type alias for a mutable mapping of event types to event handlers."""
-
 
 class EventDispatcher:
     """Dispatches events to registered handlers.
 
     Attributes:
         _handlers: Mapping of event types to event handlers.
-        _args: Arguments to pass to all event handlers.
+        _args: Arguments can be passed to all event handlers.
         _cached_args: Cached arguments to pass to event handlers.
     """
 
@@ -43,9 +39,7 @@ class EventDispatcher:
         self._handlers: _HandlersMutMapping = defaultdict(list)
 
         self._args: dict[str, Any] = {}
-        self._cached_args: dict[
-            EventHandlerType[GatewayEvent], dict[str, Any],
-        ] = {}
+        self._cached_args: dict[EventHandlerType, dict[str, Any]] = {}
 
     @overload
     def add_handler(
@@ -75,12 +69,7 @@ class EventDispatcher:
             ValueError: If the event type is not specified and cannot be inferred.
         """
         if event_handler is None:
-            if not isinstance(event_type, EventHandlerType[EVENT_T]):
-                raise TypeError(
-                    'Event handler must be specified if event type is not',
-                )
             event_handler = cast(EventHandlerType[EVENT_T], event_type)
-
             handler_arg_types = list(get_type_hints(event_handler).values())
             if not handler_arg_types:
                 raise ValueError(
@@ -92,6 +81,11 @@ class EventDispatcher:
         if not isinstance(event_type, type) or not issubclass(event_type, GatewayEvent):
             raise TypeError(
                 'Event type must be specified if the event handler is not',
+            )
+
+        if not isinstance(event_handler, Callable):
+            raise TypeError(
+                'Event handler must be Callable if the event type is not specified',
             )
 
         event_type = cast(type[EVENT_T], event_type)
@@ -121,18 +115,25 @@ class EventDispatcher:
             kwargs = self._cached_args[event_handler]
             try:
                 await event_handler(event, **kwargs)
-            except Exception as exc:
-                logger.exception(exc)
+            except Exception:
+                logger.exception('Unhandled exception in event handler')
 
-    def _update_args_cache(self, event_handler: EventHandlerType[GatewayEvent]) -> None:
+    def _update_args_cache(self, event_handler: EventHandlerType[EVENT_T]) -> None:
         """Update the arguments to pass to an event handler.
 
         Args:
             event_handler: Event handler to update.
         """
-        args = event_handler.__code__.co_varnames[1: event_handler.__code__.co_argcount]
+        arg_names = event_handler.__code__.co_varnames[1: event_handler.__code__.co_argcount]
         self._cached_args[event_handler] = {
-            arg: self._args[arg]
-            for arg in args
-            if arg in self._args
+            arg_name: self._args[arg_name]
+            for arg_name in arg_names
+            if arg_name in self._args
         }
+
+
+type _HandlersMutMapping[EVENT_T: GatewayEvent] = MutableMapping[
+    type[EVENT_T],
+    list[EventHandlerType[EVENT_T]],
+]
+"""Type alias for a mutable mapping of event types to event handlers."""
--- tests/gateway/test_dispatcher.py
+++ tests/gateway/test_dispatcher.py
@@ -19,76 +19,113 @@ class CustomEvent2(GatewayEvent):
 
 
 def test_add_handler_with_event_type(dispatcher: EventDispatcher):
-    async def handler(event: CustomEvent):
+    async def handler(_: CustomEvent):
         pass
 
     dispatcher.add_handler(handler)
-    assert GatewayEvent in dispatcher._handlers
-    assert handler in dispatcher._handlers[GatewayEvent]
+
+    assert CustomEvent in dispatcher._handlers
+    assert handler in dispatcher._handlers[CustomEvent]
+
+
+def test_add_handler_with_event_type_and_handler(dispatcher: EventDispatcher):
+    async def handler(_: CustomEvent):
+        pass
+
+    dispatcher.add_handler(CustomEvent, handler)
+
+    assert CustomEvent in dispatcher._handlers
+    assert handler in dispatcher._handlers[CustomEvent]
+
+
+def test_error_if_handler_is_not_callable(dispatcher: EventDispatcher):
+    with pytest.raises(TypeError, match='must be Callable'):
+        dispatcher.add_handler(CustomEvent, 'must be Callable')  # type: ignore
+
+    with pytest.raises(TypeError, match='must be Callable'):
+        dispatcher.add_handler('must be Callable')  # type: ignore
+
+
+def test_error_if_event_type_is_not_a_type(dispatcher: EventDispatcher):
+    with pytest.raises(TypeError, match='Event type must be specified'):
+        dispatcher.add_handler('must be a type', lambda _: None)  # type: ignore
+
+
+def test_add_handler_with_no_event_type(dispatcher):
+    with pytest.raises(ValueError, match='Event handler must have at least one argument'):
+        dispatcher.add_handler(lambda: None)  # type: ignore
+
+    with pytest.raises(ValueError, match='Event handler must have at least one argument'):
+        dispatcher.add_handler(lambda _: None)  # type: ignore
+
+
+def test_add_handler_with_invalid_event_type(dispatcher):
+    async def handler(_: mock.Mock()):  # type: ignore
+        pass
+    with pytest.raises(TypeError):
+        dispatcher.add_handler(str, handler)
 
 
 def test_add_argument(dispatcher):
-    handler = mock.Mock()
+    """Test adding an argument to the dispatcher.
+
+    Test that the argument is added to the dispatcher's arguments and
+    that it is added to the cached arguments for all handlers.
+    """
+    async def handler(_: CustomEvent, arg1: str):
+        pass
+
     dispatcher.add_handler(GatewayEvent, handler)
+    assert 'arg1' not in dispatcher._args
+    assert 'arg1' not in dispatcher._cached_args[handler]
+
     dispatcher.add_argument('arg1', 'value1')
+
     assert 'arg1' in dispatcher._args
     assert dispatcher._args['arg1'] == 'value1'
+
     assert handler in dispatcher._cached_args
     assert 'arg1' in dispatcher._cached_args[handler]
     assert dispatcher._cached_args[handler]['arg1'] == 'value1'
 
 
-@pytest.mark.asyncio
-async def test_dispatch(dispatcher, mocker):
-    handler = mocker.Mock()
-    dispatcher.add_handler(GatewayEvent, handler)
+async def test_dispatch(dispatcher):
+    """Test dispatching an event.
+
+    Test that the event is dispatched to the correct handler.
+    """
+    handler = mock.AsyncMock()
+    handler.__code__ = mock.Mock(co_argcount=2, co_varnames=['event', 'arg1'])
+
+    dispatcher.add_handler(CustomEvent, handler)
     dispatcher.add_argument('arg1', 'value1')
-    event = mocker.Mock(spec=GatewayEvent)
+
+    event = CustomEvent()
     await dispatcher.dispatch(event)
     handler.assert_called_once_with(event, arg1='value1')
 
 
-def test_add_handler_with_no_event_type(dispatcher, mocker):
-    handler = mocker.Mock()
-    dispatcher.add_handler(handler)
-    assert GatewayEvent in dispatcher._handlers
-    assert handler in dispatcher._handlers[GatewayEvent]
+async def test_dispatch_with_no_handlers(dispatcher):
+    await dispatcher.dispatch(CustomEvent)  # Should not raise any errors
 
 
-def test_add_handler_with_invalid_event_type(dispatcher, mocker):
-    handler = mocker.Mock()
-    with pytest.raises(TypeError):
-        dispatcher.add_handler(str, handler)
-
-
-def test_add_argument_with_existing_argument(dispatcher, mocker):
-    handler = mocker.Mock()
-    dispatcher.add_handler(GatewayEvent, handler)
+async def test_dispatch_with_multiple_handlers(dispatcher):
+    """Test dispatching an event with multiple handlers."""
+    handler1 = mock.AsyncMock()
+    handler1.__code__ = mock.Mock(co_argcount=1, co_varnames=['event'])
+    handler2 = mock.AsyncMock()
+    handler2.__code__ = mock.Mock(co_argcount=2, co_varnames=['event', 'arg1'])
+    handler3 = mock.AsyncMock()
+    handler3.__code__ = mock.Mock(co_argcount=1, co_varnames=['event'])
     dispatcher.add_argument('arg1', 'value1')
-    with pytest.raises(ValueError):
-        dispatcher.add_argument('arg1', 'value2')
 
+    dispatcher.add_handler(CustomEvent, handler1)
+    dispatcher.add_handler(CustomEvent, handler2)
+    dispatcher.add_handler(CustomEvent2, handler3)
 
-async def test_dispatch_with_no_handlers(dispatcher, mocker):
-    event = mocker.Mock(spec=GatewayEvent)
-    await dispatcher.dispatch(event)  # Should not raise any errors
+    event = CustomEvent()
 
-
-async def test_dispatch_with_multiple_handlers(dispatcher, mocker):
-    handler1 = mocker.Mock()
-    handler2 = mocker.Mock()
-    dispatcher.add_handler(GatewayEvent, handler1)
-    dispatcher.add_handler(GatewayEvent, handler2)
-    event = mocker.Mock(spec=GatewayEvent)
     await dispatcher.dispatch(event)
     handler1.assert_called_once_with(event)
-    handler2.assert_called_once_with(event)
-
-
-def test_update_args_cache(dispatcher, mocker):
-    handler = mocker.Mock()
-    dispatcher.add_handler(GatewayEvent, handler)
-    dispatcher.add_argument('arg1', 'value1')
-    dispatcher._update_args_cache(handler)
-    assert 'arg1' in dispatcher._cached_args[handler]
-    assert dispatcher._cached_args[handler]['arg1'] == 'value1'
+    handler2.assert_called_once_with(event, arg1='value1')
+    handler3.assert_not_called()
