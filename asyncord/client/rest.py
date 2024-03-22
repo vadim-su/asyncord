bc66cfffc6c5b8adfde84619e0429e9c31ee339a
ReRubis <slesarenkod@gmail.com>
2024-03-22 01:31:58 +0000

    Remove http protocols
    Signed-off-by: ReRubis <slesarenkod@gmail.com>

--- asyncord/client/rest.py
+++ asyncord/client/rest.py
@@ -7,8 +7,8 @@ import aiohttp
 from asyncord.client.applications.resources import ApplicationResource
 from asyncord.client.channels.resources import ChannelResource
 from asyncord.client.guilds.resources import GuildResource
+from asyncord.client.http.client import AsyncHttpClient
 from asyncord.client.interactions.resources import InteractionResource
-from asyncord.client.ports import AsyncHttpClientPort
 from asyncord.client.resources import ClientResource
 from asyncord.client.users.resources import UserResource
 
@@ -20,7 +20,7 @@ class RestClient(ClientResource):
         self,
         token: str,
         session: aiohttp.ClientSession | None = None,
-        http_client: AsyncHttpClientPort | None = None,
+        http_client: AsyncHttpClient | None = None,
     ) -> None:
         """Initialize the REST client.
 
