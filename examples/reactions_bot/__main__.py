e6f636ca384ce742bcb33fb31cfb7c2967184c20
ReRubis <slesarenkod@gmail.com>
2024-03-30 11:49:44 +0000

    Add reactions example
    Signed-off-by: ReRubis <slesarenkod@gmail.com>

---
+++ examples/reactions_bot/__main__.py
@@ -0,0 +1,38 @@
+import aiohttp  # noqa
+
+from asyncord.client.rest import RestClient
+from asyncord.gateway.client.client import GatewayClient
+from asyncord.gateway.events.base import ReadyEvent
+from examples.reactions_bot.commands import ReactionsCommand
+
+API_TOKEN = ''
+APP_ID = ''
+
+
+async def main() -> None:
+    """Run the bot."""
+    async with aiohttp.ClientSession() as session:
+        gw = GatewayClient(
+            token=API_TOKEN,
+            session=session,
+        )
+        client = RestClient(API_TOKEN)
+        client._http_client._session = session
+        gw.dispatcher.add_argument('client', client)
+        gw.dispatcher.add_argument('gateway', gw)
+
+        async def on_ready(
+            ready_event: ReadyEvent,
+            gateway: GatewayClient,
+            client: RestClient,
+        ) -> None:
+            await ReactionsCommand(APP_ID).register(gateway, client, APP_ID)
+
+        gw.dispatcher.add_handler(on_ready)
+        await gw.connect()
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(main())
