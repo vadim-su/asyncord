2be2f00ae30de5b01819db5d21178559ef986627
Vadim Suharnikov <vsuharnikov@gmail.com>
2024-06-25 19:34:44 +0000

    Refactor code and improve test coverage in scheduled events models

--- tests/integration/client/test_scheduled_events.py
+++ tests/integration/client/test_scheduled_events.py
@@ -1,5 +1,7 @@
+import contextlib
 import datetime
 from collections.abc import AsyncGenerator
+from typing import Any
 
 import pytest
 
@@ -109,3 +111,145 @@ async def test_get_event_users(
     """Test getting a list of users who have signed up for a scheduled event."""
     users = await events_res.get_event_users(event.id)
     assert isinstance(users, list)
+
+
+@pytest.mark.parametrize(
+    'event_type',
+    [pytest.param(event_type, id=event_type.name) for event_type in EventEntityType],
+)
+@pytest.mark.parametrize(
+    'model_fields',
+    [
+        pytest.param(
+            {},
+            id='without_fields',
+        ),
+        pytest.param(
+            {
+                'entity_metadata': {'location': 'https://example.com'},
+            },
+            id='with_entity_metadata',
+        ),
+        pytest.param(
+            {
+                'scheduled_end_time': datetime.datetime.now(datetime.UTC),
+            },
+            id='with_end_time',
+        ),
+        pytest.param(
+            {
+                'entity_metadata': {'location': 'https://example.com'},
+                'scheduled_end_time': datetime.datetime.now(datetime.UTC),
+            },
+            id='with_all_necessary_fields',
+        ),
+    ],
+)
+@pytest.mark.parametrize('channel_id', [None, 1234567890])
+async def test_envent_type_validation_on_creation(
+    event_type: EventEntityType,
+    model_fields: dict[str, Any],
+    channel_id: int | None,
+) -> None:
+    """Test entity type validation on creation."""
+    entity_metada: dict[str, Any] | None = model_fields.get('entity_metadata')
+    # fmt: off
+    has_fields = bool(
+        entity_metada
+        and model_fields.get('scheduled_end_time')
+        and entity_metada.get('location'),
+    )
+    # fmt: on
+
+    err_context = contextlib.nullcontext()
+
+    if event_type is EventEntityType.EXTERNAL:
+        if not has_fields:
+            err_context = pytest.raises(
+                ValueError,
+                match='EXTERNAL type requires the fields',
+            )
+
+    elif not channel_id:
+        err_context = pytest.raises(ValueError, match='`channel_id` must be set if')
+
+    with err_context:
+        CreateScheduledEventRequest(
+            name='Test Event',
+            privacy_level=EventPrivacyLevel.GUILD_ONLY,
+            scheduled_start_time=datetime.datetime.now(datetime.UTC),
+            entity_type=event_type,
+            channel_id=channel_id,
+            **model_fields,
+        )
+
+
+@pytest.mark.parametrize(
+    'event_type',
+    [pytest.param(event_type, id=event_type.name) for event_type in EventEntityType] + [None],
+)
+@pytest.mark.parametrize(
+    'model_fields',
+    [
+        pytest.param(
+            {},
+            id='without_fields',
+        ),
+        pytest.param(
+            {
+                'entity_metadata': {'location': 'https://example.com'},
+            },
+            id='with_entity_metadata',
+        ),
+        pytest.param(
+            {
+                'scheduled_end_time': datetime.datetime.now(datetime.UTC),
+            },
+            id='with_end_time',
+        ),
+        pytest.param(
+            {
+                'entity_metadata': {'location': 'https://example.com'},
+                'scheduled_end_time': datetime.datetime.now(datetime.UTC),
+            },
+            id='with_all_necessary_fields',
+        ),
+    ],
+)
+@pytest.mark.parametrize('channel_id', [None, 1234567890])
+async def test_envent_type_validation_on_updating(
+    event_type: EventEntityType | None,
+    model_fields: dict[str, Any],
+    channel_id: int | None,
+) -> None:
+    """Test entity type validation on updating.
+
+    It differs from the creation test because event type can be None for updating.
+    """
+    entity_metada: dict[str, Any] | None = model_fields.get('entity_metadata')
+    # fmt: off
+    has_fields = bool(
+        entity_metada
+        and model_fields.get('scheduled_end_time')
+        and entity_metada.get('location'),
+    )
+    # fmt: on
+
+    err_context = contextlib.nullcontext()
+
+    if event_type is EventEntityType.EXTERNAL:
+        if not has_fields:
+            err_context = pytest.raises(
+                ValueError,
+                match='EXTERNAL type requires the fields',
+            )
+
+    elif event_type and not channel_id:
+        err_context = pytest.raises(ValueError, match='`channel_id` must be set if')
+
+    with err_context:
+        UpdateScheduledEventRequest(
+            entity_type=event_type,
+            channel_id=channel_id,
+            **model_fields,
+        )
